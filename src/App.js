bb1c9a6e133312b0b6a4521c6590fc373e3c4583
Samer A <thedev.samer@gmail.com>
2022-03-09 11:11:23 +0200

    added new css files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return <div>TODO App</div>;
-  }
+function App() {
+  const [todos, setTodos] = useState([
+    "Take dogs for a walk",
+    "Take the rubbish out",
+    "Qazi wants to livestream today",
+  ]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, input.trim()]);
+    setInput("");
+  };
+
+  return (
+    <div className="App">
+      <h1>TODO App</h1>
+      <form>
+        <input
+          value={input}
+          onChange={(event) => setInput(event.currentTarget.value)}
+        />
+        <button type="submit" onClick={addTodo}>
+          Add Todo
+        </button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
